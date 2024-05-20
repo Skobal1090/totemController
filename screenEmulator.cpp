6a467b671c58ef27f8c568bfdb0e904d10587b8c
Seth <skobal1090@gmail.com>
2024-05-20 01:07:59 -0400

    Render data from buffer to screen

--- screenEmulator.cpp
+++ screenEmulator.cpp
@@ -3,17 +3,48 @@
 #include <unistd.h>
 using namespace std;
 
+
+
 void printToScreen(){
-    int i = 0;
+    int f = 0;
     int seconds = 0;
+    char buffer[8][32] = {};
+    //gets size of array
+    int rows = sizeof buffer / sizeof buffer[0];
+    int cols = sizeof buffer[0] / sizeof(char);
+    for (int x = 0; x < rows; x ++){
+        for (int y = 0; y < cols; y ++){
+            buffer[x][y] = ' ';
+        }
+    }
+
     while(true){
+        //Ensures everything is printed to cout before clearing screen
+        cout << flush;
         //Invokes system "CLS" command to wipe screen 
         system("CLS");
-        cout << seconds <<flush;
+
+        //Prints array
+        for (int i = 0; i < rows; i ++){
+            for (int j = 0; j < cols; j ++){
+                cout << buffer[i][j];
+            } cout << endl;
+        }
+
+        cout << "\nSeconds Passed: " << seconds << endl;
+        cout << "\nFrames Passed: " << f << endl;
+
+        //Sets next buffer to be printed (temp)
+        for (int x = 0; x < rows; x ++){
+            for (int y = 0; y < cols; y ++){
+                (f % cols == y) ? buffer[x][y] = '*': buffer[x][y] = ' ';
+        }
+    }
+
         //usleep takes microseconds as an argument, we chose a refresh rate of 5hz
-        usleep(200000);
-        if (i % 5 == 0) seconds++;
-        i++;
+        usleep(50000);
+        if (f % 20 == 0) seconds++;
+        f++;
     }
 }
 
