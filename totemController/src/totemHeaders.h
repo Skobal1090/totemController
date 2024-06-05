25486554904c40bdaa4b99c647fa151258ae4874
Peter Mullins <jc310xc@gmail.com>
2024-06-04 21:09:53 -0400

    update totemHeaders to include new color providers

--- totemController/src/totemHeaders.h
+++ totemController/src/totemHeaders.h
@@ -2,3 +2,5 @@
 #include <screen/colorInterpolator.h>
 #include <screen/colorProvider.h>
 #include <screen/solidColorProvider.h>
+#include <screen/randomColorProvider.h>
+#include <screen/twoColorSwapProvider.h>
